9e3988263e31e8c78b5d36b18e154addc51846b3
EllenSuni <ellen.suni@outlook.com>
2023-04-25 16:39:00 +0200

    Created footer component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,12 @@
+import Footer from "./Footer";
+
 function App() {
   return (
     <div className="App">
       <h1>Group Project</h1>
+      <div>
+        <Footer/>
+      </div>
     </div>
   );
 }
