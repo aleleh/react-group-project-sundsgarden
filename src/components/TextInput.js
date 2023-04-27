7305e3c9d2a9fff0962888055061fc3460c2baf4
EllenSuni <ellen.suni@outlook.com>
2023-04-27 21:27:36 +0200

    Input gets stored

--- src/components/TextInput.js
+++ src/components/TextInput.js
@@ -1,12 +1,22 @@
+import { useState } from "react";
+
 const TextInput = () => {
-    
+    // variable to store input
+    const [input, setinput] = useState("");
+
+    // function to set input
+    const handleInput = (e) => {
+        setinput(e.target.value);
+    };
+
 
     return (
         <div>
             <div>
                 <label>Write here</label>
-                <input type="text"/>
+                <input type="text" name="input" value={input} onChange={handleInput}/>
             </div>
+            <h5>{input}</h5>
         </div>
     );
 };
