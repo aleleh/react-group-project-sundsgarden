50dcc2775051ab548466c6e80c8e8fac2d1187b5
aleleh <alexander.lehnbom@gmail.com>
2023-04-28 10:53:34 -0400

    minor changes
    minor changes. Deleted unneccasary imports

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Home";
 import Header from "./Header";
 import Footer from "./Footer";
-import Signup from "./Signup";
-import Login from "./Login";
 
 function App() {
   return (
