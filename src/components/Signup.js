3c15c3e13696f3d6ea877b94d8367b2cea8c7d87
AumDigital <dave.church721@gmail.com>
2023-05-02 12:27:35 +0200

    added notes to sign up

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -2,31 +2,39 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 const Signup = () => {
+  /* Declares three state variables - all with an empty string from start*/
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const checkEmail = (users) => {
+    /* This function uses the find method to check for a matching  email address in users*/
     const user = users.find((user) => user.email === email);
     if (user) return user;
   };
 
   const handleSubmit = async (event) => {
+    /* an asynchronus function with an event as its argument*/
     event.preventDefault();
-
+    /* prevent default stops the refreshing of the page after submit */
     const user = await axios
+    /* uses axios to find useres in /users*/
       .get("/users")
+    /* This function checks if the email variable matches any of the returned email objects found */
       .then((res) => checkEmail(res.data, email));
 
     if (user) {
+      /* if a truthy value returns alert below */
       alert("User already exists!");
     } else {
+      /* if false, creates a new user object and displays alert */
       const user = { name, email, password };
       axios.post("/users", user).then(alert("User created!"));
     }
   };
 
   return (
+    /* standard sign up form */
     <div className="form-container">
       <div className="input-container">
         <h3> Sign Up!</h3>
@@ -38,6 +46,8 @@ const Signup = () => {
             name="name"
             value={name}
             id="name"
+            /* inline onChange variable to handle form input */
+            
             onChange={(e) => setName(e.target.value)}
             placeholder="Full Name"
           />
@@ -61,7 +71,7 @@ const Signup = () => {
             onChange={(e) => setPassword(e.target.value)}
             placeholder="Password"
           />
-
+          
           <button type="submit" className="submit-btn" onSubmit={handleSubmit}>
             Sign Up!
           </button>
