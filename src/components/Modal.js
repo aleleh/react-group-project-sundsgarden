73c4e0fa845a657c9aec69efe6cddd5c8cdfe36a
AumDigital <dave.church721@gmail.com>
2023-05-03 16:13:27 +0200

    close functionality

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,16 +1,25 @@
 import Signup from "./Signup";
 import Login from "./Login";
 import { MdClose } from "react-icons/md";
+import { useState } from "react";
 
-function Modal({children, closeModal}) {
+function Modal() {
+  const [isModalVisible, setIsModalVisible] = useState(true);
+  const handleExit = () => {
+    setIsModalVisible(false);
+  };
+
   return (
-    <div className="overlay">
-      <div className="content">
-        <Signup />
-        <Login />
-        <MdClose className="close" onClick={closeModal} />
-        {children}
-      </div>
+    <div>
+      {isModalVisible && (
+        <>
+          <button onClick={handleExit} className="exit-btn">
+            Exit
+          </button>
+          <Signup />
+          <Login />
+        </>
+      )}
     </div>
   );
 }
