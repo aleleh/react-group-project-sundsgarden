d53b9ea1d94a8b19bfe38f710a7f405ac0c4481d
EllenSuni <ellen.suni@outlook.com>
2023-05-03 17:55:06 +0200

    Created nextSnippet function

--- src/components/DisplayCode.js
+++ src/components/DisplayCode.js
@@ -7,17 +7,18 @@ const DisplayCode = () => {
     const [index, setIndex] = useState(0);
     const [snippet, setSnippet] = useState("");
     
-
-
-    const btnClicked = (e) => {
-        e.preventDefault();
-        console.log("clicked");
+    const nextSnippet = () => {
         if (index < codeSnippets.length) {
             setIndex(index + 1);
             setSnippet(codeSnippets[index]);
         } else {
             setSnippet("No more snippets");
         }
+    };
+
+    const btnClicked = (e) => {
+        e.preventDefault();
+        nextSnippet();
     };
 
     
--- src/data/CodeSnippets.js
+++ src/data/CodeSnippets.js
@@ -1,16 +1,8 @@
 const codeSnippets = [
-    {
-        id: 1,
-        code: 'const variable = "hello";'
-    },
-    {
-        id: 2,
-        code: 'let number = 45;'
-    },
-    {
-        id: 3,
-        code: 'function Name() {};'
-    }
+    'const variable = "hello";',
+    'let number = 45;',
+    'function Name() {};'
+    
 ]
 
 export default codeSnippets;
