a9092caa1e8eb89be9edd580a3cf553ddf4ee35b
AumDigital <dave.church721@gmail.com>
2023-05-12 15:55:47 +0200

    compare function resolved

--- src/components/Compare.js
+++ src/components/Compare.js
@@ -2,69 +2,68 @@ import codeSnippets from "../data/CodeSnippets";
 import { useState, useRef, useEffect } from "react";
 import Game from "./Game";
 
-
 const Compare = (props) => {
+  const [error, setError] = useState(false);
+  const inputRef = useRef(null);
 
-    const [error, setError] = useState(false);
-    const [button, setButton] = useState('check');
-    const inputRef = useRef(null);
-    
-    const [index, setIndex] = useState(0);
-        const [snippet, setSnippet] = useState("");
-        
-        const nextSnippet = () => {
-            setError(false);
-            setButton('Next');
-            
-            if (index < codeSnippets.length) {
-                setIndex(index + 1);
-                setSnippet(codeSnippets[index]);
-            } else {
-                setSnippet("No more snippets");
-            }
-        };
+  const [index, setIndex] = useState(0);
+  const [snippet, setSnippet] = useState("");
 
-        useEffect(() => {
-            if (props.gameStarted) {
+  const handleSubmit = (e) => {
+    // Function to compare user input to code snippet //
+    e.preventDefault();
+    const insertedText = inputRef.current.value;
+    if (insertedText === snippet) {
+      nextSnippet();
+    // Clears input field //
+      inputRef.current.value = "";
+    } else {
+        // Display error message //
+      setError(true);
+    }
+  };
 
-            // This function needs to be written outside of the use effect hook to work properly and compare the input to the code //
-        
-            const insertedText = inputRef.current.value;
-            if (insertedText === snippet) {
-            nextSnippet();
-            inputRef.current.value = '';
-            } else {
-            setError(true);
-            setButton('Try Again');
-            nextSnippet();
-        }
-        }
-     }, [props.gameStarted]);
-
-    
+  const nextSnippet = () => {
+    setError(false);
 
 
+    inputRef.current.value = "";
+    if (index < codeSnippets.length - 1) {
+        // using prevState to update the state as it wasn't working properly before and not updating correctly.  This enables the state to be updated and sets the state correctly. 
+      setIndex(prevIndex => prevIndex + 1);
+      setSnippet(codeSnippets[index + 1]);
+    } else {
+      setSnippet("No more snippets");
+    }
+  };
 
-    return (
-        <div>
-            <div id="word" className="Home">
-            <h2>Typing Game</h2>
-            <small>Type the following:</small>
-            <p>{snippet}</p>
-            
+  useEffect(() => {
+    //Passes gameStarted as a prop from game component, if true sets displays first snippet in codeSnippets array //
+    if (props.gameStarted) {
+      setSnippet(codeSnippets[0]);
+    }
+  }, [props.gameStarted]);
 
-            <input
+  return (
+    <div>
+      <div id="word" className="Home">
+        <h2>Typing Game</h2>
+        <small>Type the following:</small>
+        <p>{snippet}</p>
+
+        <form onSubmit={handleSubmit}>
+          <input
             type="text"
             autoComplete="off"
             placeholder="Type the word here..."
             ref={inputRef}
-            />
-            {error && <p className="error-message">Please try again</p>}
-            {/* <button onClick={btnClicked}>{button}</button> */}
-            </div>
-            
-        </div>
-    );
+          />
+          {error && <p className="error-message">Please try again</p>}
+         
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Compare;
