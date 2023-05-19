a72572bf7f878add857fd3ea53b965f8b3e0c599
Sara Talal <sara.talal99.st@gmail.com>
2023-05-20 01:08:02 +0200

    add a login.js file

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from "./Home";
 import SignIn from "./SignIn";
 import LogIn from "./LogIn";
-import Account from './Account';
 import Game from './Game';
 
 function App() {
@@ -13,7 +12,6 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/SignIn" element={<SignIn/>}/>
           <Route path="/LogIn" element={<LogIn />} />
-          <Route path="/Account" element={<Account />} />
           <Route path="/Game" element={<Game />} />
         </Routes>
       </BrowserRouter>
