5cf8afa3b0ae954ff714fd1a6c999e20753b34d2
EllenSuni <ellen.suni@outlook.com>
2023-05-23 15:45:40 +0200

    Play Game btn is opening Game

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,10 @@
+import Game from "./Game";
+import {Link} from "react-router-dom";
+
 const Home = () => {
   return (
     <div className="home-container">
-      <p className="btn big">PLAY GAME!</p>
+      <Link to="/Game" className="btn big">PLAY GAME!</Link>
     </div>
   );
 };
